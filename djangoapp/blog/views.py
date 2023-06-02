983dc06307b7f73783ccd792b0bb1c8056e1f8b4
Henrique Almeida <rick.vieira.almeida@gmail.com>
2023-06-01 23:11:49 -0300

    feat: add HTML and CSS to the blog
    The basis of the visual part of the blog was added
    with some Django logic, with test posts
    posts that will be modified in the future

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,5 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+posts = list(range(18))
 
 
 def index(request):
-    return render(request, 'blog/pages/index.html')
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/pages/index.html', {'page_obj': page_obj, })
+
+
+def page(request):
+    return render(request, 'blog/pages/page.html',)
+
+
+def post(request):
+    return render(request, 'blog/pages/post.html')
