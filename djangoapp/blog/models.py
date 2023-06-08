de69aa32fff17ab1ea18abd339eb8e3de81da3c9
Henrique Almeida <rick.vieira.almeida@gmail.com>
2023-06-08 20:22:11 -0300

    feat: create the model post and put the relations
    Relationships must be made between the Tag and
    Category models that will be added to the Post
    as the foreign key.

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from utils.rands import slugify_new
 
@@ -51,7 +52,7 @@ class Page(models.Model):
     )
     is_published = models.BooleanField(
         default=False, help_text=(
-            'This field must be checked in order for the'
+            'This field must be checked in order for the '
             'page to be publicly displayed.'
         )
     )
@@ -64,3 +65,49 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, default=None, null=True, blank=True, max_length=70
+    )
+    is_published = models.BooleanField(
+        default=False, help_text=(
+            'If checked, it will display the cover inside the post.'
+        )
+    )
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts_%Y/%m/', blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True, help_text=(
+            'If checked, it will display the cover inside the post.'
+        )
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='post_created_by'
+    )
+    updated_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='post_updated_by'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
